550292fe91cf6a68edea05588963b707937954f8
Todd Nordquist <toddnordquist144@gmail.com>
2022-03-09 15:28:29 -0700

    Add methods and test in process.

--- binary-trees/src/main/java/com/toddnordquist/BinaryTree.java
+++ binary-trees/src/main/java/com/toddnordquist/BinaryTree.java
@@ -1,5 +1,8 @@
 package com.toddnordquist;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BinaryTree {
 
   Node root;
@@ -42,4 +45,58 @@ public class BinaryTree {
   public boolean containsNode(int value) {
     return containsNodeRecursive(root, value);
   }
+
+  private Node deleteRecursive(Node current, int value) {
+
+    if (current == null) {
+      return null;
+    }
+
+
+    if (value == current.value) {
+
+      if (current.left == null && current.right == null) {
+        return null;
+      }
+
+      if (current.right == null) {
+        return current.left;
+      }
+
+      if (current.left == null) {
+        return current.right;
+      }
+
+      int smallestValue = findSmallestValue(current.right);
+      current.value = smallestValue;
+      current.right = deleteRecursive(current.right, smallestValue);
+      return current;
+    }
+    if (value < current.value) {
+      current.left = deleteRecursive(current.left, value);
+    }
+
+    current.right = deleteRecursive(current.right, value);
+    return current;
+  }
+
+  private int findSmallestValue(Node root) {
+    return root.left == null ? root.value : findSmallestValue(root.left);
+  }
+
+  public void delete(int value) {
+    root = deleteRecursive(root,value);
+  }
+
+    List<Integer> orderedList = new LinkedList<>();
+  public List<Integer> traverseInOrder(Node node) {
+    if (node != null) {
+      traverseInOrder(node.left);
+      orderedList.add(node.value);
+      System.out.println(" " + node.value);
+      traverseInOrder(node.right);
+    }
+    return orderedList;
+  }
+
 }
--- binary-trees/src/test/java/com/toddnordquist/BinaryTreeTest.java
+++ binary-trees/src/test/java/com/toddnordquist/BinaryTreeTest.java
@@ -1,19 +1,45 @@
 package com.toddnordquist;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BinaryTreeTest {
 
+  private BinaryTree bt;
+
+  @BeforeEach
+  void setUp() {
+    bt = createBinaryTree();
+  }
+
   @Test
   public void givenABinaryTree_whenAddingElements_thenTreeContainsThoseElements() {
 
-    BinaryTree bt = createBinaryTree();
     assertTrue(bt.containsNode(6));
     assertTrue(bt.containsNode(7));
     assertFalse(bt.containsNode(1));
+  }
 
+  @Test
+  public void givenABinaryTree_whenDeletingElements_thenTreeDoesNotContainThoseElements() {
+    assertTrue(bt.containsNode(6));
+    bt.delete(6);
+    assertFalse(bt.containsNode(6));
+    assertEquals(7, bt.root.value);
+  }
+
+  @Test
+  public void givenABinaryTree_traversingInOrder_showValuesInOrder() {
+    List<Integer> list = new LinkedList<>(Arrays.asList(3, 4, 5, 6, 7, 8, 9));
+    assertEquals(list, Collections.unmodifiableList(bt.traverseInOrder(bt.root)));
   }
 
   private BinaryTree createBinaryTree() {
